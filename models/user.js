cab92d1367bc27c66105128858018b8b7bd3d1c1
Lukasz Kaczmarczyk <lukaszkaczmarczyk1996@gmail.com>
2019-12-05 18:00:41 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,5 @@
-//---------------------------------------------- Michal --------------------
-// const config = require('config');
-// const jwt = require('jsonwebtoken');
-//--------------------------------------------------------------------------
-
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi)
 const mongoose = require('mongoose');
@@ -26,18 +23,15 @@ const userSchema = new mongoose.Schema({
     },
     character_id: {
       type: ObjectId,
+      ref: 'Characters',
       default: null
     }
 });
 
-//----------------------------------- Michał ------------------------------------------
-
-// userSchema.methods.generateAuthToken = function() { 
-//     const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
-//     return token;
-//   }
-
-//-------------------------------------------------------------------------------------
+userSchema.methods.generateAuthToken = function() { 
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
+}
 
 const User = mongoose.model('User', userSchema);
 
