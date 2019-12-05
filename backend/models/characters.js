a01c65990c262002d9c30925af80e01f4f75d25b
Mateusz <mateusz.fordas@gmail.com>
2019-12-05 17:06:10 +0100

    test

--- backend/models/characters.js
+++ backend/models/characters.js
@@ -1,11 +1,10 @@
 const Joi = require('joi');
-const {
-    Schema
-} = require('mongoose');
-const ObjectId = Schema.Types.ObjectId;
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 
-const Character = new Schema({
+const characterSchema = new mongoose.Schema({
     _id: {
         type: Number
     },
@@ -14,7 +13,8 @@ const Character = new Schema({
         ref: 'Guild'
     },
     inventory_id: {
-        type: Number
+        type: ObjectId,
+        ref: 'Inventory'
     },
     questbook_id: {
         type: ObjectId,
@@ -27,28 +27,34 @@ const Character = new Schema({
         maxlength: 255
     },
     level: {
-        type: Number
+        type: Number,
+        default: 1
     },
     class: {
         enum: [warrior, wizzard]
     },
     exp_points: {
-        type: Number
+        type: Number,
+        default: 0
     },
     hit_points: {
-        type: Number
+        type: Number,
+        default: 0
     },
     physical_power: {
-        type: Number
+        type: Number,
+        default: 0
     },
     magical_power: {
-        type: Number
+        type: Number,
+        default: 0,
+        required: true
     },
 
 });
 
 function validateCharacter(character) {
-    const schema = {
+    const schema = Joi.object({
         name: Joi.string().min(5).max(50).required(),
         guild_id: Joi.objectId(),
         questbook_id: Joi.objectId().required(),
@@ -60,10 +66,12 @@ function validateCharacter(character) {
         physical_power: Joi.number().min(0).required(),
         magical_power: Joi.number().min(0).required(),
 
-    };
+    });
 
-    return Joi.validate(character, schema);
+    return schema.validate(character);
 }
+
+const Character = mongoose.model('Character', characterSchema);
 
 exports.Character = Character;
 exports.validate = validateCharacter;
--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const express = require('express');
 const app = express();
 
 //-----------------------------------------------------------
-const {User, validate} = require('./models/user'); 
+const {
+    User,
+    validate
+} = require('./models/user');
+const {
+    Character,
+    validate
+} = require('./models/characters');
 const mongoose = require('mongoose');
 //-----------------------------------------------------------
 
@@ -18,38 +25,77 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static('public'));
 app.use(helmet());
 app.use('/', home);
 
-if(app.get('env') == 'development') {
+if (app.get('env') == 'development') {
     app.use(morgan('tiny'));
     debug('Morgan enabled...');
 }
 
 //----------------------------------------------
 
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/test', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => console.log('Conneted'))
     .catch(err => console.log("Error"))
 
 
 app.post('/user', async (req, res) => {
-    const { error } = validate(req.body);
+    const {
+        error
+    } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-  
-    let user = await User.findOne({ email: req.body.email });
+
+    let user = await User.findOne({
+        email: req.body.email
+    });
     if (user) return res.status(400).send('User already registered.');
-  
+
     user = new User(req.body);
     console.log(user);
     await user.save();
 
     res.send(req.body);
-  });
+});
 
-  //------------------------------------------------
+//------------------------------------------------
+
+//----------------------------------------------
+
+mongoose.connect('mongodb://localhost/test', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Conneted'))
+    .catch(err => console.log("Error"))
+
+
+app.post('/characters', async (req, res) => {
+    const {
+        error
+    } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let character = await Character.findOne({
+        magical_power: req.body.magical_power
+    });
+    if (character) return res.status(400).send('User already registered.');
+
+    character = new Character(req.body);
+    console.log(character);
+    await character.save();
+
+    res.send(req.body);
+});
+
+//------------------------------------------------
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
