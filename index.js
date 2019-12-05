22364ffcda6749e1d4ca9325a678964a2aa37a24
Mateusz <mateusz.fordas@gmail.com>
2019-12-05 16:32:10 +0100

    test

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const home = require('./backend/routes/home');
 const express = require('express');
 const app = express();
 
-require('./backend/startup/prod')(app);
+//-----------------------------------------------------------
+const {User, validate} = require('./models/user'); 
+const mongoose = require('mongoose');
+//-----------------------------------------------------------
+
+require('./startup/prod')(app);
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -22,6 +27,29 @@ if(app.get('env') == 'development') {
     app.use(morgan('tiny'));
     debug('Morgan enabled...');
 }
+
+//----------------------------------------------
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Conneted'))
+    .catch(err => console.log("Error"))
+
+
+app.post('/user', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
+  
+    user = new User(req.body);
+    console.log(user);
+    await user.save();
+
+    res.send(req.body);
+  });
+
+  //------------------------------------------------
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
