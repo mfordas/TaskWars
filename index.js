7eee9111f1772ee152461529421202b879faf8fb
Lukasz Kaczmarczyk <lukaszkaczmarczyk1996@gmail.com>
2019-12-05 05:01:01 +0100

    testy

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
+const {User, validate} = require('./models/user'); //
+const mongoose = require('mongoose');
+
+
 require('./startup/prod')(app);
 
 app.set('view engine', 'pug');
@@ -22,6 +26,27 @@ if(app.get('env') == 'development') {
     app.use(morgan('tiny'));
     debug('Morgan enabled...');
 }
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Conneted'))
+    .catch(err => console.log("Error"))
+
+//
+app.post('/user', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
+  
+    user = new User(req.body);
+    console.log(user);
+    await user.save();
+
+    res.send(req.body);
+  });
+
+  //
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
