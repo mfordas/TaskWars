a44234ed644e4b9566431a23bace1f4ee64819a9
Martyna <martynalecka.ml@gmail.com>
2019-12-08 17:23:23 +0100

    Added connection to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config({ path: '.env' });
 const debug = require('debug')('app:startup');
 const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('@hapi/joi');
 const home = require('./backend/routes/home');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
@@ -18,10 +20,15 @@ app.use(express.static('public'));
 app.use(helmet());
 app.use('/', home);
 
+const mongodbURI = process.env.MONGODB_URI;
+mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+.then(() => console.log('Connect to MongoDB...'))
+.catch((error) => console.log('Cannot connect with database.',error));
+
 if(app.get('env') == 'development') {
     app.use(morgan('tiny'));
     debug('Morgan enabled...');
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
