ed7e9e990e6cfbdcce6664ca1cb2e7ebeb9d6ed1
Mateusz <mateusz.fordas@gmail.com>
2019-12-10 22:36:25 +0100

    app.js changes

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,28 @@
-const config = require('config');
 const debug = require('debug')('app:startup');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const mongoose = require('mongoose');
 const home = require('./routes/home');
-const users = require('./routes/users');
 const express = require('express');
 const path = require('path');
 const db = require('./db');
 
-if(!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not definded.');
-  process.exit(1);
-  // set TaskWars_jwtPrivateKey=mySecureKey
-}
-
 const main = async () => {
   const app = express();
 
-  // if (app.get('env') == 'development') {
-  //   app.use(morgan('tiny'));
-  //   debug('Morgan enabled...');
-  // }
+  if (app.get('env') == 'development') {
+    app.use(morgan('tiny'));
+    debug('Morgan enabled...');
+  }
 
-  // // Database configuration
-  // const connection = await db.connect();
-  // const models = db.load(connection);
-  // if (process.env.TEST_ENV || process.env.NODE_ENV) {
-  //   await connection.dropDatabase();
-  //   await db.initialize(models);
-  // }
+  // Database configuration
+  const connection = await db.connect();
+  const models = db.load(connection);
+  if (process.env.TEST_ENV || process.env.NODE_ENV) {
+    await connection.dropDatabase();
+    await db.initialize(models);
+  }
 
-  // db.register(app, connection, models);
-
-  mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+  db.register(app, connection, models);
 
   //Middlewares
   app.use(express.json());
@@ -45,16 +32,11 @@ const main = async () => {
 
   //Routes
   app.use('/', home);
-  app.use('/api/users', users);
 
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-  // //Listening
-  // const host = process.env.HOST || '127.0.0.1';
-  // const port = process.env.PORT || 8080;
-  // app.listen(port, host, () => console.log(`Listening on http://${host}:${port}`));
+  //Listening
+  const host = process.env.HOST || '127.0.0.1';
+  const port = process.env.PORT || 8080;
+  app.listen(port, host, () => console.log(`Listening on http://${host}:${port}`));
 };
-console.log('cls');
-main();
 
+main();
