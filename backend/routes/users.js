43c1ae635317ec918e2338f3661119dd6d737098
Michał <michal14300@gmail.com>
2019-12-11 15:36:37 +0100

    users endpoints GET/, GET/:id, PUT/:id/password, PUT/me/password

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
+const auth = require('../middleware/authorization');
 const express = require('express');
 const router = express.Router();
 
@@ -21,6 +22,49 @@ router.post('/', async (req, res) => {
 
   const token = user.generateAuthToken();
   res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+router.get('/', async (req, res) => {
+  const users = await User.find().sort('_id');
+  res.send(_.pick(users, ['_id', 'name', 'email']));
+});
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) return res.status(404).send('The user with the given ID was not found.');
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+router.put('/:id/password', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const salt = await bcrypt.genSalt(10);
+  const newPassword = await bcrypt.hash(req.body.password, salt);
+
+  const user = await User.findByIdAndUpdate(req.params.id, { password: newPassword }, {
+    new: true
+  });
+  if (!user) return res.status(404).send('The user with the given ID was not found.');
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+router.put('/me/password', auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const salt = await bcrypt.genSalt(10);
+  const newPassword = await bcrypt.hash(req.body.password, salt);
+
+  const user = await User.findByIdAndUpdate(req.user._id, { password: newPassword }, {
+    new: true
+  });
+  if (!user) return res.status(404).send('The user with the given ID was not found.');
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
