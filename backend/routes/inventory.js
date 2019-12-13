268ea8eae650a61edf7264cd8cb8b4ca9b364f02
Martyna <martynalecka.ml@gmail.com>
2019-12-13 18:04:36 +0100

    Improved routes item and inventory: added put/equipped

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,21 +1,28 @@
-const {inventory, validateInventory} = require('../models/inventory');
-const {item, validateItem} = require('../models/item');
+const { validateInventory } = require('../models/inventory');
+const { validateItemID } = require('../models/item');
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const Inventory = mongoose.model('inventories', inventory);
-const Item = mongoose.model('items', item);
+
 
 router.get('/:id', async (req, res) => {
-    const inventory = await Inventory.findById(req.params.id);
+    const Inventory = res.locals.models.inventory;
+    const inventory = await Inventory.findById(req.params.id)
+    .catch( err => { 
+        console.error(`Bad request. The given ID: ${req.params.id} was not valid. ${err}`); 
+        return null; 
+    });
     if(!inventory) return res.status(404).send('The inventory with the given ID was not found.');
     res.send(inventory);
 });
 
 router.put('/:id/gold', async (req, res) => {
+    const Inventory = res.locals.models.inventory;
     const { error } = validateInventory(req.body.inventory);
     if(error) return res.status(400).send(error.details[0].message);
-
+    
+    if(req.body.inventory == null || req.body.inventory.gold == null  ) 
+        return res.status(400).send('Bad request: none inventory/gold value in request body.');
+    
     const inventory = await Inventory.findByIdAndUpdate(req.params.id,
         {
             gold: req.body.inventory.gold
@@ -25,16 +32,40 @@ router.put('/:id/gold', async (req, res) => {
 });
 
 router.put('/:id/backpack', async (req, res) => {
-    const item = await Item.findById(req.body._id);
-    if(!item) return res.status(400).send('Invalid item.');
+    const Inventory = res.locals.models.inventory;
+    const Item = res.locals.models.item;
 
-    const inventoryHandel = await Inventory.findById(req.params.id, 'backpack', { lean: true });
+    if(req.body._id == null) 
+        return res.status(400).send('Bad request: none item._id value in request body.');
+
+    const { error } = validateItemID(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const item = await Item.findById(req.body._id)
+        .catch( err => { 
+            console.error(`Bad request. The given ID: ${req.body._id} was not valid. ${err}`); 
+            return null; 
+        });
+    if(!item) return res.status(400).send(`Item with given id ${req.body._id} was not found`);
+
+    const inventoryHandel = await Inventory.findById(req.params.id, 'backpack', { lean: true })
+        .catch( err => { 
+            console.error(`Bad request. The given ID: ${req.params.id} was not valid. ${err}`); 
+            return null; 
+        });
+    if(inventoryHandel === null) 
+        return res.status(404).send(`Bad request. The given ID: ${req.params.id} was not valid.`);
+    
     inventoryHandel.backpack.push(req.body._id);
 
     const inventory = await Inventory.findByIdAndUpdate(req.params.id,
         {
             backpack: inventoryHandel.backpack
-        }, { new: true });
+        }, { new: true })
+        .catch( err => { 
+            console.error(`Bad request. The given ID: ${req.params.id} was not valid. ${err}`); 
+            return null; 
+        });
 
     if(!inventory) return res.status(404).send('The inventory with the given ID was not found.');
     res.send(inventory);
