8a1dfe8ab63a494ae8e30007339daee15dab7e3f
Michał <michal14300@gmail.com>
2019-12-13 20:53:26 +0100

    auth update / bcryptjs lib

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 const Joi = require('@hapi/joi');
 const bcrypt = require('bcryptjs');
 const _ = require('lodash');
-const { User } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+  const User = res.locals.models.user;
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
