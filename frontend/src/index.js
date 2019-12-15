5e635370ce2d675b1e4301c1122a7d02b9deaf06
Szymon <szymonpakula98@gmail.com>
2019-12-16 00:20:12 +0100

    Frontend initial commit
    TODO: styles, sass,

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>{
+    return(
+        <div>
+            <h1>Frontend stands ready!</h1>
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
