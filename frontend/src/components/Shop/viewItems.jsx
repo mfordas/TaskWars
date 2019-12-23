02f744dc1539b76f2064cf146fc9fbc66b8376af
Martyna <martynalecka.ml@gmail.com>
2019-12-23 23:09:43 +0100

    Added current gold value

--- frontend/src/components/Shop/viewItems.jsx
+++ frontend/src/components/Shop/viewItems.jsx
@@ -3,7 +3,7 @@ import { Segment, Icon, Item, Button, Image, ItemExtra  } from 'semantic-ui-reac
 import setHeaders from '../../utils/setHeaders';
 
 class ViewItems extends React.Component {
-  state = { items: [] }
+  state = { items: [], id_user: 0, id_inventory: 0 , gold: 0 }
 
   fetchItems = async () => {
     const response = await fetch('/api/item', setHeaders());
@@ -12,13 +12,49 @@ class ViewItems extends React.Component {
     console.log(body);
     this.setState({ items: body });
   }
+
+  fetchUser = async () => {
+    const response = await fetch('/api/users/me', setHeaders());
+    console.log(response);
+    const body = await response.json();
+    console.log(body);
+    this.setState({ id_user: body.character_id });
+    this.fetchInventory(this.state.id_user);
+  }
+
+  fetchInventory = async (character_id) => {
+    const response = await fetch('/api/characters/'+character_id, setHeaders());
+    console.log(response);
+    const body = await response.json();
+    console.log(body);
+    this.setState({ id_inventory: body.inventory_id });
+    this.fetchUserGold(this.state.id_inventory);
+  }
+
+  fetchUserGold = async (inventory_id) => {
+    const response = await fetch('/api/inventory/'+inventory_id, setHeaders());
+    console.log(response);
+    const body = await response.json();
+    console.log(body);
+    this.setState({ gold: body.gold });
+
+  }
+
   componentDidMount() {
     this.fetchItems();
+    this.fetchUser();
+
     console.log('mounted');
+  }
+  componentDidUpdate() {
+
+
   }
   render() {
     return (
+      
       <Segment.Group vertical>
+        <Segment>Your gold: { this.state.gold }</Segment>
         <Item.Group>
         {this.state.items.map(item => (
 
