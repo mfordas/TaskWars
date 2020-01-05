35c4ac7c6f75ecea1f727edd5ba61919e94796ca
Martyna <martynalecka.ml@gmail.com>
2020-01-05 20:47:46 +0100

    Changes in app.js - add few lines

--- backend/app.js
+++ backend/app.js
@@ -46,6 +46,11 @@ const main = async () => {
   app.use('/api/tasks', tasks);
   app.use('/api/guilds', guilds);
   app.use('/api/questbook', questbook);
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    });
 
   //Listening
   const host = process.env.HOST || '127.0.0.1';
